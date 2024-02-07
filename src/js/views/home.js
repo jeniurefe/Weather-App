c5bffb6c42a2462e75e4557ab3dfbf9e3ea26281
Jenipher <128754089+jeniurefe@users.noreply.github.com>
2024-02-07 01:11:07 +0000

    Se realizo el container y el input

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -3,13 +3,24 @@ import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
 export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
+
+	<div className="container">
+		<div className="search-box">
+			<i className="fas fa-map-marker-alt"></i>
+		 	<input type="text" placeholder="enter your location"></input>
+			<button className="fas fa-search"></button>
+		</div>
+
+		<div className="weather-box">
+			<div className="box">
+				<div className="info-weather">
+					<div className="weather">
+						<img src=""></img>
+					</div>
+				</div>
+
+			</div>
+
+		</div>
 	</div>
 );
