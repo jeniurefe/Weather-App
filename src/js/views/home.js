aa73781525425b5e4140f912b79862ffc8155e0c
Jeni <jenurefer2111@gmail.com>
2024-10-04 18:03:35 -0600

    Cambios

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,86 @@
 import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
 export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
+	<div className="container">
+		<div className="current-info">
+
+			<div className="date-container">
+				<div className="time" id="time">
+					12.30 <span id=""am-pm>PM</span>
+				</div>
+				<div className="date" id="date">
+					Monday, 24 june
+				</div>
+
+				<div className="others" id="current-weather-items">
+					<div className="weather-item">
+						<div>Humidity</div>
+						<div>95.2%</div>
+					</div>
+					<div className="weather-item">
+						<div>Pressure</div>
+						<div>121</div>
+					</div>
+					<div className="weather-item">
+						<div>Wind Speed</div>
+						<div>222</div>
+					</div>
+				</div>
+			</div>
+
+			<div className="place-container">
+				<div className="time-zone" id="time-zone">Asia/Kolkata</div>
+				<div id="country" className="country">IN</div>
+			</div>
+		</div>
+
+		<div className="future-forecast">
+			<div className="today" id="current-temp">
+				<img src="" alt="weather icon" className="w-icon"></img>
+				<div className="day">Monday</div>
+				<div className="temp">Night - 25.6&#176; C</div>
+				<div className="temp">Day - 35.6&#176; C</div>
+			</div>
+
+			<div className="weather-forecast" id="weather-forecast">
+				<div className="weather-forecast-item">
+					<div className="day">Tuesday</div>
+					<img src="" alt="weather icon" className="w-icon"></img>
+					<div className="temp">Night - 25.6&#176; C</div>
+					<div className="temp">Day - 35.6&#176; C</div>
+				</div>
+
+				<div className="weather-forecast-item">
+					<div className="day">Wed</div>
+					<img src="" alt="weather icon" className="w-icon"></img>
+					<div className="temp">Night - 25.6&#176; C</div>
+					<div className="temp">Day - 35.6&#176; C</div>
+				</div>
+
+				<div className="weather-forecast-item">
+					<div className="day">Thur</div>
+					<img src="" alt="weather icon" className="w-icon"></img>
+					<div className="temp">Night - 25.6&#176; C</div>
+					<div className="temp">Day - 35.6&#176; C</div>
+				</div>
+
+				<div className="weather-forecast-item">
+					<div className="day">Fri</div>
+					<img src="" alt="weather icon" className="w-icon"></img>
+					<div className="temp">Night - 25.6&#176; C</div>
+					<div className="temp">Day - 35.6&#176; C</div>
+				</div>
+
+				<div className="weather-forecast-item">
+					<div className="day">Sat</div>
+					<img src="" alt="weather icon" className="w-icon"></img>
+					<div className="temp">Night - 25.6&#176; C</div>
+					<div className="temp">Day - 35.6&#176; C</div>
+				</div>
+
+			</div>
+		</div>
+	
 	</div>
 );
